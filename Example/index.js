a7a52d0d5f7444a14aa58694436834975e2d8c95
rgled002 <rgled002@gold.ac.uk>
2021-12-06 11:53:16 +0000

    Tidy up

--- Example/index.js
+++ Example/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-use-before-define, max-classes-per-file */
 
-import Engine from './Engine.js';
-import Rectangle from './Rectangle.js';
-import Vector2 from './Vector2.js';
+import Engine from '../engine/Engine.js';
+import Rectangle from '../engine/Rectangle.js';
+import Vector2 from '../engine/Vector2.js';
 
 window.onload = () => {
   new DemoGame();
@@ -35,19 +35,18 @@ class DemoGame {
         position: new Vector2(10, 10), width: 50, height: 50, colour: 'red', zIndex: 1,
       },
     );
-    this.player.grow = this.grow.bind(this);
+    this.player.grow = () => {
+      this.player.width += 1;
+      this.player.height += 1;
+    };
 
     this.game = new Engine(this.update.bind(this), {
       title: 'Demo Game',
       backgroundColour: 'black',
     });
-
-    this.#frames = 0;
   }
 
   update() {
-    this.#frames += 1;
-
     this.player.position = new Vector2(
       this.game.mouseX - this.player.width / 2,
       this.game.mouseY - this.player.height / 2,
@@ -59,10 +58,5 @@ class DemoGame {
         this.player.grow();
       }
     });
-  }
-
-  grow() {
-    this.player.width += 1;
-    this.player.height += 1;
   }
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const router = express();
 
 router.use('/', express.static(path.join(__dirname, 'public')));
 router.use('/engine', express.static(path.join(__dirname, 'engine')));
+router.use('/Example', express.static(path.join(__dirname, 'Example')));
 
 router.listen(3000, () => {
   console.log('Server running on port 3000');
